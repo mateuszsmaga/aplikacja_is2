08f3a0b6608f70acdd2a96a29309a2e8a2f9df6e
Mateusz <mateusz.smaga@gmail.com>
2016-12-02 09:44:30 +0100

    psw

--- src/main/java/main/WebSecurityConfig.java
+++ src/main/java/main/WebSecurityConfig.java
@@ -32,6 +32,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth
             .inMemoryAuthentication()
-                .withUser("user").password("some_password").roles("USER");
+                .withUser("aaaaaa").password("aaaaaa").roles("USER");
     }
 }
